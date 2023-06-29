b4941a2b3bcbbc8ce96ed7f0f442659a7aca2001
Fanzil Nabiyarov <ifanzilka@MacBook-Air-Fanzil.local>
2023-06-29 03:26:36 +0300

    add 404 page

--- webserver/includes/WebServHeaders.hpp
+++ webserver/includes/WebServHeaders.hpp
@@ -14,13 +14,21 @@ font-family: Tahoma, Verdana, Arial, sans-serif; }\
 </head>\
 <body>\
 <h1>Welcome to ifanzilka WebServ!</h1>\
-<p>If you see this page, the  web server is successfully installed and\
+<p>If you see this page, the  web server is successfully installed and \
 working. Further configuration is required.</p>\
 <p>For online documentation and support please refer to\
-<a href=\"https://github.com/ifanzilka/WebServer/\"> github/ifanzilka/webserv</a>.<br/>\
+<a href=\"https://github.com/ifanzilka/WebServer/\">   github/ifanzilka/webserv</a>.<br/>\
 <p><em>Thank you for using my WebServ.</em></p>\
 </body>\
 </html>"
 
+
+#define PAGE_404 "<html>\
+    <head><title>404 Not Found</title></head>\
+    <body>\
+    <center><h1>404 Not Found</h1></center>\
+    <hr><center>WebServ/v1</center>\
+    </body>\
+    </html>"
 
 #endif
